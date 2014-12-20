9db6ac33702b9239a6355a1c3d90d87784524ea0
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2014-12-19 16:15:36 -0800

    more interesting code I think

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Flinching-Robot
 ===============
 
 Tessel-powered robot car that flinches away from loud noises.
+
+I wanted to see if I could make a robot that reacts to its environment in an emotionally compelling way. This one is based on the idea that 
--- index.js
+++ index.js
@@ -9,37 +9,31 @@ require('tesselate') ({
   var right = modules.ambientR;
   var car = require('servo-car').use(tessel.port['B'], function () {
     // Start the device (define trigger values in this function, below)
-    reset();
+    left.setSoundTrigger(0.1243);
+    right.setSoundTrigger(0.1113);
+
+    car.stop(); // Just to rev the motors
     console.log('Just sitting here minding my own business...');
 
     // When we get a sound, on either side, run away
     left.on('sound-trigger', function(data) {
-      left.clearSoundTrigger();
       console.log('Left triggered', data);
       car.right();
       setTimeout(function () {
         setTimeout(function () {
-          reset();
+          car.stop();
         }, 500);
       }, 500);
     });
     right.on('sound-trigger', function (data) {
-      right.clearSoundTrigger();
       console.log('Right triggered', data);
       car.left();
       setTimeout(function () {
         car.back();
         setTimeout(function () {
-          reset();
+          car.stop();
         }, 500);
       }, 500);
     });
-
-    // Once we've reacted, stop running and wait for triggers again
-    function reset () {
-      car.stop();
-      left.setSoundTrigger(0.1243);
-      right.setSoundTrigger(0.1113);
-    }
   });
 });
